6523eed1c5137a12f8cad378e398510ef5143768
Lola Barbier <lolabarbier@hotmail.com>
2018-02-19 17:42:15 +0100

    forgot before action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :find_student, only: [ :show, :create, :edit, :update, :destroy]
+  before_action :find_student, only: [ :show, :edit, :update, :destroy]
 
   def index
     @students = Student.all
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params)
     if @student.save
       redirect_to students_path
     else
