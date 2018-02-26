97e69afb264cd915202032bc36bdf6c24ca4d658
Lola Barbier <lolabarbier@hotmail.com>
2018-02-26 18:38:20 +0100

    disguised links, removed preventDefault

--- app/javascript/components/displayEditForm.js
+++ app/javascript/components/displayEditForm.js
@@ -24,13 +24,13 @@ function displayEditForm() {
   // })
 
   const students = document.getElementById("students");
-
-
-    students.addEventListener("click", function (event) {
-      event.preventDefault();
+  students.addEventListener("click", function (event) {
+    if (event.target.parentNode.parentNode.parentNode.childNodes[5].classList.contains("form_space")) {
       const editBox = event.target.parentNode.parentNode.parentNode.childNodes[5];
       editBox.classList.toggle("invisible");
-    })
+      console.log(event.target);
+    }
+  })
 }
 
 export { displayEditForm };
