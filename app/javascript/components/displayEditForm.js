f7ba6d3be9c83c4469133d95379311c7f7e9ade0
Lola Barbier <lolabarbier@hotmail.com>
2018-02-26 18:18:57 +0100

    event listener works on new dom item

--- app/javascript/components/displayEditForm.js
+++ app/javascript/components/displayEditForm.js
@@ -9,16 +9,28 @@ function displayEditForm() {
   //     student.childNodes[3].classList.toggle("invisible");
   //   });
   // })
-  const students = document.querySelectorAll('.student');
+  // const students = document.querySelectorAll('.student');
 
-  students.forEach((student) => {
-    student.addEventListener("click", function (event) {
-      if (event.target.classList.contains('fa')) {
-        event.preventDefault();
-        student.childNodes[5].classList.toggle("invisible");
-      }
+  // students.forEach((student) => {
+  //   student.addEventListener("click", function (event) {
+  //     event.preventDefault();
+  //     console.log(student);
+  //     if (event.target.classList.contains('fa')) {
+  //       console.log("hi")
+
+  //       student.childNodes[5].classList.toggle("invisible");
+  //     }
+  //   })
+  // })
+
+  const students = document.getElementById("students");
+
+
+    students.addEventListener("click", function (event) {
+      event.preventDefault();
+      const editBox = event.target.parentNode.parentNode.parentNode.childNodes[5];
+      editBox.classList.toggle("invisible");
     })
-  })
 }
 
 export { displayEditForm };
